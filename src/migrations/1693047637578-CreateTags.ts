90b0246a7f11e59bbae324be68474623f835789d
Hemant Goyal <hgoyal@shoptrue.com>
2023-08-26 16:41:57 +0530

    First Commit

---
+++ src/migrations/1693047637578-CreateTags.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateTags1693047637578 implements MigrationInterface {
+    name = 'CreateTags1693047637578'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "tags" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_e7dc17249a1148a1970748eda99" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "tags"`);
+    }
+
+}
---
+++ src/ormconfig.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+console.log(__dirname);
+export default new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'iamhemant',
+  database: 'medium-clone',
+  synchronize: false,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+});
